eec3d0568bb9749568f1e1bcf2fda45d7a66c163
David Skoog <david.g.skoog@knowit.se>
2025-12-25 10:48:46 +0100

    Implement InfoFromWedding block. Also update Hero and Timeline.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import Hero from './src/components/hero/hero';
+import Hero from './src/components/hero/Hero.jsx';
 import './App.css';
-import Info from './src/components/info/Info';
-import Program from './src/components/program/Program';
-import Header from './src/components/header/Header.jsx';
 import Gallery from './src/components/gallery/Gallery.jsx';
 import Fab from './src/components/fab/Fab.jsx';
 import MediaPlayer from './src/components/media-player/MediaPlayer.jsx';
 import Footer from './src/components/footer/Footer.jsx';
+import InfoFromWedding from './src/components/info-from-wedding/InfoFromWedding.jsx';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
       <Hero />
-      <Program />
+      <InfoFromWedding />
       <Gallery />
-      <Info />
       <MediaPlayer />
       <Footer />
       <Fab />
